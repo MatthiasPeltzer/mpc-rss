f07e6acfb97ceafd81cf417510eb7694391b9def
Matthias <mail@mpeltzer.de>
2026-07-17 07:40:16 +0200

    Test infrastructure: runTests.sh, CI matrix, Codeception acceptance
    Add Docker runTests.sh runner, multi-DB functional CI, TYPO3 13/14 quality matrix, Codeception backend acceptance tests, and mp-core CI fixes (PostgreSQL postgres user, Docker on GHA, TYPO3_PATH split, testing-framework ^9.3).

---
+++ Tests/Acceptance/Support/Extension/BackendEnvironment.php
@@ -0,0 +1,36 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Mpc\MpcRss\Tests\Acceptance\Support\Extension;
+
+use TYPO3\TestingFramework\Core\Acceptance\Extension\BackendEnvironment as TestingFrameworkBackendEnvironment;
+
+final class BackendEnvironment extends TestingFrameworkBackendEnvironment
+{
+    protected $localConfig = [
+        'coreExtensionsToLoad' => [
+            'typo3/cms-core',
+            'typo3/cms-backend',
+            'typo3/cms-frontend',
+            'typo3/cms-extbase',
+            'typo3/cms-fluid',
+            'typo3/cms-install',
+            'typo3/cms-scheduler',
+        ],
+        'testExtensionsToLoad' => [
+            'mpc/mpc-rss',
+        ],
+        'csvDatabaseFixtures' => [
+            __DIR__ . '/../../Fixtures/BackendEnvironment.csv',
+        ],
+        'configurationToUseInTestInstance' => [
+            'SYS' => [
+                'encryptionKey' => '0123456789abcdef0123456789abcdef0123456789abcdef0123456789abcdef',
+            ],
+            'GFX' => [
+                'processor' => 'GraphicsMagick',
+            ],
+        ],
+    ];
+}
